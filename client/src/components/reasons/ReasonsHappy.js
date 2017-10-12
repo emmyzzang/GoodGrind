264754b6862101a3bfadfeee586dc943fea46add
Sofia Wicker <sofiazw@gmail.com>
2017-10-12 11:41:36 -0400

    minor update

--- client/src/components/reasons/ReasonsHappy.js
+++ client/src/components/reasons/ReasonsHappy.js
@@ -23,21 +23,21 @@ class ReasonsHappy extends React.Component {
 
     return (
 
-    <div className='td-form {NavbarStyle}'>
+      <div className='td-form {NavbarStyle}'>
 
-      <button onClick={() => {this.reply_click("Great Culture")}} type="button" className="btn btn-lg reason" value={"Great Culture"}> Great Culture </button>
-      <button onClick={() => {this.reply_click("Flexible Managers")}} type="button" className="btn btn-lg reason" value={"Flexible Managers"}> Flexible Managers </button>
-      <button onClick={() => {this.reply_click("Friendly Co-workers")}} type="button" className="btn btn-lg reason" value={"Friendly Co-workers"}> Friendly Co-workers </button>
-      <button onClick={() => {this.reply_click("Upbeat Work Environment")}} type="button" className="btn btn-lg reason" value={"Upbeat Work Environment"}> Upbeat Work Environment </button>
-      <button onClick={() => {this.reply_click("Work is Appreciated")}} type="button" className="btn btn-lg reason" value={"Work is Appreciated"}> Work is Appreciated </button>
-      <button onClick={() => {this.reply_click("Flexible Work Hours")}} type="button" className="btn btn-lg reason" value={"Flexible Work Hours"}> Flexible Work Hours </button>
-      <button onClick={() => {this.reply_click("New Opportunities")}} type="button" className="btn btn-lg reason" value={"New Opportunities"}> New Opportunities </button>
+        <button onClick={() => {this.reply_click("Great Culture")}} type="button" className="btn btn-lg reason" value={"Great Culture"}> Great Culture </button>
+        <button onClick={() => {this.reply_click("Flexible Managers")}} type="button" className="btn btn-lg reason" value={"Flexible Managers"}> Flexible Managers </button>
+        <button onClick={() => {this.reply_click("Friendly Co-workers")}} type="button" className="btn btn-lg reason" value={"Friendly Co-workers"}> Friendly Co-workers </button>
+        <button onClick={() => {this.reply_click("Upbeat Work Environment")}} type="button" className="btn btn-lg reason" value={"Upbeat Work Environment"}> Upbeat Work Environment </button>
+        <button onClick={() => {this.reply_click("Work is Appreciated")}} type="button" className="btn btn-lg reason" value={"Work is Appreciated"}> Work is Appreciated </button>
+        <button onClick={() => {this.reply_click("Flexible Work Hours")}} type="button" className="btn btn-lg reason" value={"Flexible Work Hours"}> Flexible Work Hours </button>
+        <button onClick={() => {this.reply_click("New Opportunities")}} type="button" className="btn btn-lg reason" value={"New Opportunities"}> New Opportunities </button>
 
-      <button onClick={() => {updateReasons(this.state.reasons)}}>
-            Submit
-      </button>
-    </div>
-  )
+        <button onClick={() => {updateReasons(this.state.reasons)}}>
+              Submit
+        </button>
+      </div>
+    )
   }
 };
 
