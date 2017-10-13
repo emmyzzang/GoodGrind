e28e3dd03378e2555ab72448dc735b1a0e252e19
Emmy Emme <masteremmy.js@gmail.com>
2017-10-12 22:41:29 -0400

    Moved faces; /dashboard component code => /update component

--- client/src/components/update/Update.js
+++ client/src/components/update/Update.js
@@ -1,20 +1,55 @@
 import React, { Component } from 'react';
+import { Link } from "react-router-dom";
+import UpdateStyle from './Update.css';
+import Navbar from '../navbar/Navbar.js'
+import updateFeelings from '../calls/updateFeelings.js'
+import FontAwesome from 'react-fontawesome';
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   Link
+// } from 'react-router-dom'
 
+// MEGAMAN
 class Update extends Component {
-  state = {
-    fieldOne: '',
-    fieldTwo: '',
-    fieldThree: ''
-  }
 
 render() {
+
   return (
 
-    <div className='td-form'>
-      Update Page
+    <div className='container {UpdateStyle}'>
+
+      <div className='faces'>
+
+        <ul id = 'faceList'>
+          <li className = 'face' >
+            <Link to="/happy">
+            <FontAwesome name='smile-o'
+              onClick={(event) => {updateFeelings('+1');}
+              }/>
+            </Link>
+          </li>
+
+          <li className = 'face' >
+              <Link to="/meh">
+                <FontAwesome name='meh-o'
+                  onClick={(event) => {updateFeelings('0');}
+                }/>
+              </Link>
+            </li>
+
+          <li className = 'face' >
+            <Link to="/sad">
+              <FontAwesome name='frown-o'
+                onClick={(event) => {updateFeelings('-1');}
+                }/>
+            </Link>
+          </li>
+        </ul>
+      </div>
     </div>
-      );
-    }
+    );
   }
+}
 
 export default Update;
