4ad531c7cdb41edf6c629148973452edc965ea12
Emmy Emme <masteremmy.js@gmail.com>
2017-10-15 01:52:27 -0400

    Recharts test

--- client/src/components/goals/Goals.js
+++ client/src/components/goals/Goals.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
-
+import { LineChart, Line } from 'recharts';
 
 class Goals extends Component {
   state = {
     fieldOne: '',
     fieldTwo: '',
-    fieldThree: ''
+    fieldThree: [0,1,2,3,4,5,6,7,8]
   }
 
 render() {
@@ -13,6 +13,13 @@ render() {
 
     <div className='td-form'>
       Goals Page
+
+      <LineChart width={400} height={400} data={this.state.fieldThree}>
+      <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+      </LineChart>
+
+
+
     </div>
       );
     }
--- client/src/components/stats/Stats.js
+++ client/src/components/stats/Stats.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import API from "../../util/axiosApi.js";
 
-
-
 /////////
 
 // whitebaording
