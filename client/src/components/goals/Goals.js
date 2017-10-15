00dccb2112b0a87ed0f90ebf26ce522620fe8a71
Emmy Emme <masteremmy.js@gmail.com>
2017-10-15 16:31:47 -0400

    spacetime chart init; fix the zoom and date format

--- client/src/components/goals/Goals.js
+++ client/src/components/goals/Goals.js
@@ -2,60 +2,56 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { LineChart, Line } from 'recharts';
 import { render } from 'react-dom';
-import { VictoryChart, VictoryLabel, VictoryAxis, VictoryLine, VictoryScatter, VictoryVoronoiContainer, VictoryGroup, VictoryTooltip } from 'victory';
+import { VictoryChart, VictoryLabel, VictoryAxis, VictoryLine, VictoryScatter,
+  VictoryVoronoiContainer, VictoryGroup, VictoryTooltip, VictoryZoomContainer,
+  VictoryBrushContainer} from 'victory';
 
 class Goals extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  handleZoom(domain) {
+    this.setState({selectedDomain: domain});
+  }
+
+  handleBrush(domain) {
+    this.setState({zoomDomain: domain});
+  }
+
   render() {
     return (
-      <VictoryChart height={200} width={400}
-        containerComponent={<VictoryVoronoiContainer/>}
-      >
-          <VictoryGroup
-            color="#c43a31"
-            labels={(d) => `y: ${d.y}`}
-            labelComponent={
-              <VictoryTooltip
-                style={{fontSize: 10}}
-              />
-            }
-            data={[
-              {x: 1, y: -3},
-              {x: 2, y: 5},
-              {x: 3, y: 3},
-              {x: 4, y: 0},
-              {x: 5, y: -2},
-              {x: 6, y: -2},
-              {x: 7, y: 5}
-            ]}
-          >
-            <VictoryLine/>
-            <VictoryScatter
-              size={(d, a) => {return a ? 8 : 3;}}
+      <div>
+        <VictoryChart width={400} height={200} scale={{x: "time"}}
+          containerComponent={
+            <VictoryZoomContainer
+              zoomDimension="x"
+              zoomDomain={this.state.zoomDomain}
+              onZoomDomainChange={this.handleZoom.bind(this)}
             />
-          </VictoryGroup>
-          <VictoryGroup
-            labels={(d) => `y: ${d.y}`}
-            labelComponent={
-              <VictoryTooltip
-                style={{fontSize: 10}}
-              />
-            }
-            data={[
-              {x: 1, y: 3},
-              {x: 2, y: 1},
-              {x: 3, y: 2},
-              {x: 4, y: -2},
-              {x: 5, y: -1},
-              {x: 6, y: 2},
-              {x: 7, y: 3}
-            ]}
-          >
-            <VictoryLine/>
-            <VictoryScatter
-              size={(d, a) => {return a ? 8 : 3;}}
+          }
+        >
+            <VictoryLine
+              style={{
+                data: {stroke: "cadetblue"}
+              }}
+              data={[
+                {a: new Date(2017, 10, 1), b: -1},
+                {a: new Date(2017, 10, 2), b: 0},
+                {a: new Date(2017, 10, 3), b: 1},
+                {a: new Date(2017, 10, 4), b: 0},
+                {a: new Date(2017, 10, 5), b: -1},
+                {a: new Date(2017, 10, 6), b: 1},
+                {a: new Date(2017, 10, 7), b: 1},
+                {a: new Date(2017, 10, 8), b: 1}
+              ]}
+              x="a"
+              y="b"
             />
-          </VictoryGroup>
-       </VictoryChart>
+
+          </VictoryChart>
+      </div>
     );
   }
 }
