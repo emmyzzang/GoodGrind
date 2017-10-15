6ae3c783eb47227fcbb0734418aac4f316b69a3a
Emmy Emme <masteremmy.js@gmail.com>
2017-10-15 16:05:08 -0400

    Voronoi container test -- do we like this better?

--- client/src/components/goals/Goals.js
+++ client/src/components/goals/Goals.js
@@ -2,74 +2,60 @@ import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import { LineChart, Line } from 'recharts';
 import { render } from 'react-dom';
-import { VictoryChart, VictoryLabel, VictoryAxis, VictoryLine, VictoryScatter } from 'victory';
-
-const data = [
-  {x: 'M', y: 0},
-  {x: 'T', y: 0.5},
-  {x: 'W', y: 1.0},
-  {x: 'Th', y: 1.5},
-  {x: 'F', y: 2.0},
-];
-
-const cartesianInterpolations = [
-  "basis",
-  "bundle",
-  "cardinal",
-  "catmullRom",
-  "linear",
-  "monotoneX",
-  "monotoneY",
-  "natural",
-  "step",
-  "stepAfter",
-  "stepBefore"
-];
-
-const polarInterpolations = [
-  "basis",
-  "cardinal",
-  "catmullRom",
-  "linear"
-];
-
-const InterpolationSelect = ({ currentValue, values, onChange }) => (
-  <select onChange={onChange} value={currentValue} style={{ width: 75 }}>
-    {values.map(
-      (value) => <option value={value} key={value}>{value}</option>
-    )}
-  </select>
-);
+import { VictoryChart, VictoryLabel, VictoryAxis, VictoryLine, VictoryScatter, VictoryVoronoiContainer, VictoryGroup, VictoryTooltip } from 'victory';
 
 class Goals extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      interpolation: "linear",
-      polar: false
-    };
-  }
   render() {
     return (
-      <div>
-        <InterpolationSelect
-          currentValue={this.state.interpolation}
-          values={this.state.polar ? polarInterpolations : cartesianInterpolations }
-          onChange={(event) => this.setState({ interpolation: event.target.value })}
-        />
-        <br />
-        <label htmlFor="polar">polar</label>
-        <VictoryChart polar={this.state.polar} height={200}>
-          <VictoryLine
-            interpolation={this.state.interpolation} data={data}
-            style={{ data: { stroke: "#80cbc4"} }}
-          />
-          <VictoryScatter data={data}
-            size={5}
-            style={{ data: { fill: "#80cbc4"} }}
-          />
-        </VictoryChart>
-      </div>
+      <VictoryChart height={200} width={400}
+        containerComponent={<VictoryVoronoiContainer/>}
+      >
+          <VictoryGroup
+            color="#c43a31"
+            labels={(d) => `y: ${d.y}`}
+            labelComponent={
+              <VictoryTooltip
+                style={{fontSize: 10}}
+              />
+            }
+            data={[
+              {x: 1, y: -3},
+              {x: 2, y: 5},
+              {x: 3, y: 3},
+              {x: 4, y: 0},
+              {x: 5, y: -2},
+              {x: 6, y: -2},
+              {x: 7, y: 5}
+            ]}
+          >
+            <VictoryLine/>
+            <VictoryScatter
+              size={(d, a) => {return a ? 8 : 3;}}
+            />
+          </VictoryGroup>
+          <VictoryGroup
+            labels={(d) => `y: ${d.y}`}
+            labelComponent={
+              <VictoryTooltip
+                style={{fontSize: 10}}
+              />
+            }
+            data={[
+              {x: 1, y: 3},
+              {x: 2, y: 1},
+              {x: 3, y: 2},
+              {x: 4, y: -2},
+              {x: 5, y: -1},
+              {x: 6, y: 2},
+              {x: 7, y: 3}
+            ]}
+          >
+            <VictoryLine/>
+            <VictoryScatter
+              size={(d, a) => {return a ? 8 : 3;}}
+            />
+          </VictoryGroup>
+       </VictoryChart>
     );
   }
 }
