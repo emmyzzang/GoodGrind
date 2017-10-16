0f823cd877f56757e2050e0ca183389dbea04035
Emmy Emme <masteremmy.js@gmail.com>
2017-10-15 23:57:24 -0400

    Add Reducers & Actions

---
+++ client/src/actions/index.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+import { browserHistory } from 'react-router';
+import {
+  AUTH_USER,
+  UNAUTH_USER,
+  AUTH_USER,
+  FETCH_MESSAGE
+} from './types';
+
+const ROOT_URL = 'http://localhost:8080';
+
+export function signinUser({email password}) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signin`, {email, password})
+      .then(response => {
+          dispatch({ type: AUTH_USER });
+          localStorage.setItem('token', response.data.token);
+          browserHistory.push('/feature'); // TODO - Change to Dashboard
+          // TODO ^-- Use this to redirect user to dashboard
+      })
+      .catch((error) => {
+        dispatch(authError('Bad Login Info'));
+      });
+  }
+}
+
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, {email, password})
+      .then(response => {
+          dispatch({ type: AUTH_USER });
+          localStorage.setItem('token', response.data.token);
+          browserHistory.push('/feature'); // TODO - Change to Dashboard
+      })
+      .catch( error => dispatch(authError(error.response.data.error)));
+  }
+}
+
+export function authError(error) {
+  return {
+    type: AUTH_ERROR,
+    payload: error
+  };
+}
+
+export function signoutUser() {
+  localStorage.removeItem('token');
+  return { type: UNAUTH_USER };
+}
+
+export function fetchMessage(){
+  return function(dispatch) {
+    axios.get(ROOT_URL, {
+      headers: { authorization: localStorage.getItem('token') }
+    })
+    .then(response => {
+      dispatch({
+        type: FETCH_MESSAGE,
+        payload: response.data.message
+      })
+    });
+  }
+}
