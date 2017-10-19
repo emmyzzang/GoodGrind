219e7bb93845e457bc9e9a1975da680634b73f64
Emmy Emme <masteremmy.js@gmail.com>
2017-10-19 00:17:03 -0400

    Data gives me life

--- client/src/components/stats/Stats.js
+++ client/src/components/stats/Stats.js
@@ -110,6 +110,28 @@ class Stats extends Component {
 
           </VictoryChart>
       </div>
+
+    // TODO - Fix a lot of shit. Note to self: stop being a noob scrub with state.
+    //   <div>
+    //    <VictoryChart height={400} width={400}
+    //      domainPadding={{x: 50, y: [0, 20]}}
+    //      scale={{x: "time"}}
+    //    >
+    //      <VictoryBar
+    //        dataComponent={
+    //          <Bar events={{ onMouseOver: handleMouseOver }}/>
+    //        }
+    //        style={this.state.style}
+    //        data={[
+    //          {x: new Date(1986, 1, 1), y: 2},
+    //          {x: new Date(1996, 1, 1), y: 3},
+    //          {x: new Date(2006, 1, 1), y: 5},
+    //          {x: new Date(2016, 1, 1), y: 4}
+    //        ]}
+    //      />
+    //    </VictoryChart>
+    //  </div>
+
     );
   }
 }
